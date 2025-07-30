47edb509351ba876092b601577c2999c481713e4
Ivo_Ricardo <ivo.junior@ufcspa.edu>
2025-07-30 17:03:12 -0300

    atualizei

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+
+
 function Home() {
-  return <h1> Teste </h1>; 
+  return (
+    <div className="container">
+      <h1 className="main-message">Duda te amo</h1>
+      <p className="sub-message">Esta é a minha forma de dizer que estou aprendendo a programar para te impressionar. ❤️</p>
+    </div>
+  );
 }
 
 export default Home;
